9fe6cd0137db3a7d53d616339bd24f8fb5c03d8a
Asuman Aydın <asuman.aydin6709@gmail.com>
2020-03-31 21:06:28 +0300

    Add files via upload
    Start

---
+++ TextEditor/src/ClientListener.java
@@ -0,0 +1,34 @@
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+
+
+public class ClientListener{
+	public static int sendUserNametoServer(String username)
+	{
+		
+		return 0;
+		
+	}
+	
+	public static int sendPasswordtoServer(String password)
+	{
+		return 0;
+	}
+	
+	public static void writeToFile(int version, int lineNo, String text)
+	{
+		
+	}
+	public static String appendTheFile(int version, String text)
+	{
+		return "";
+	}
+	//update 
+	public static void exit()
+	{
+		
+	}
+}
