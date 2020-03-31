9d503a44b3cb7598edb0ea76aa23982c26c7a1d8
Asuman Aydın <asuman.aydin6709@gmail.com>
2020-03-31 23:52:19 +0300

    Add files via upload

---
+++ TextEditor.java
@@ -0,0 +1,19 @@
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+
+
+public class TextEditor{
+	
+	public static void main(String args[]) throws IOException
+	{
+		String Addr = args[0];
+		String ControlPort = args[1];
+		
+		
+	}
+	
+
+}
