0300c24c81aa87a12510ad84f28de6b16759b192
Asuman Aydın <asuman.aydin6709@gmail.com>
2020-04-07 20:18:06 +0300

    Submitted.

--- TextEditor.java
+++ TextEditor.java
@@ -4,8 +4,18 @@ import java.net.Socket;
 import java.io.*;
 import java.util.*;
 
+/**
+ * Collaborative Text Editing program used TCP protocol includes one main and 4
+ * other functions
+ * 
+ * @author Asuman Aydın & Muhammad Bin Sanaullah
+ * @version 1.0
+ * @since 2020-04-7
+ */
+
 public class TextEditor {
 
+	// functionalities used in communication btw client and server
 	private static PrintWriter writer = null;
 	private static BufferedReader fromServer = null;
 	private static InputStream inputStream = null;
@@ -13,13 +23,21 @@ public class TextEditor {
 	private static Socket socketClient = null;
 	private static Scanner in = null;
 
-	//Authentication
+	// Authentication
 	private static Boolean userAuth = false;
 	private static Boolean passAuth = false;
 
-	//Version of the file
+	// Version of the file
 	int version;
 
+	/**
+	 * This is the main method
+	 * 
+	 * @param args used to take localhost and port
+	 * @return Nothing.
+	 * @exception IOException On input errors.
+	 * @see IOException
+	 */
 	public static void main(String args[]) throws IOException {
 
 		TextEditor myObje = new TextEditor();
@@ -27,7 +45,7 @@ public class TextEditor {
 		String ipaddress = args[0]; // = "localhost";
 		int port = Integer.parseInt(args[1]);// = 60000;
 
-		//Connect to the server
+		// Connect to the server
 		socketClient = new Socket(ipaddress, port);
 		System.out.println("Connection is started.");
 
@@ -36,24 +54,20 @@ public class TextEditor {
 		inputStreamRe = new InputStreamReader(inputStream);
 		fromServer = new BufferedReader(inputStreamRe);
 		writer = new PrintWriter(socketClient.getOutputStream(), true);
-		in  = new Scanner(System.in);
+		in = new Scanner(System.in);
 
 		// USER AUTH
 		System.out.print("Username Command:");
 		String userName = in.nextLine();
-		if(userName.contains("EXIT"))
-		{
-			try
-			{
+		if (userName.contains("EXIT")) {
+			try {
 				writer.close();
 				fromServer.close();
 				inputStream.close();
 				inputStreamRe.close();
 				socketClient.close();
 				in.close();
-			}
-			catch(IOException exc)
-			{
+			} catch (IOException exc) {
 				System.out.println("Exception while exiting: " + exc.getMessage());
 			}
 			System.out.println("Connection is closed.");
@@ -61,17 +75,14 @@ public class TextEditor {
 		}
 		userAuth = sendUserCommand(userName);
 		if (userAuth == false) {
-			try
-			{
+			try {
 				writer.close();
 				fromServer.close();
 				inputStream.close();
 				inputStreamRe.close();
 				socketClient.close();
 				in.close();
-			}
-			catch(IOException exc)
-			{
+			} catch (IOException exc) {
 				System.out.println("Exception while exiting: " + exc.getMessage());
 			}
 			System.out.println("Connection is closed.");
@@ -80,19 +91,15 @@ public class TextEditor {
 		// PASS AUTH
 		System.out.print("Password Command:");
 		String password = in.nextLine();
-		if(password.contains("EXIT"))
-		{
-			try
-			{
+		if (password.contains("EXIT")) {
+			try {
 				writer.close();
 				fromServer.close();
 				inputStream.close();
 				inputStreamRe.close();
 				socketClient.close();
 				in.close();
-			}
-			catch(IOException exc)
-			{
+			} catch (IOException exc) {
 				System.out.println("Exception while exiting: " + exc.getMessage());
 			}
 			System.out.println("Connection is closed.");
@@ -100,17 +107,14 @@ public class TextEditor {
 		}
 		passAuth = sendPassCommand(password);
 		if (passAuth == false) {
-			try
-			{
+			try {
 				writer.close();
 				fromServer.close();
 				inputStream.close();
 				inputStreamRe.close();
 				socketClient.close();
 				in.close();
-			}
-			catch(IOException exc)
-			{
+			} catch (IOException exc) {
 				System.out.println("Exception while exiting: " + exc.getMessage());
 			}
 			System.out.println("Connection is closed.");
@@ -119,101 +123,87 @@ public class TextEditor {
 		String[] response = { "OK", "INVALID" };
 		System.out.println("Please enter a command to execute: ");
 		String command = in.nextLine();
-		if(command.contains("EXIT"))
-		{
-			try
-			{
+		if (command.contains("EXIT")) {
+			try {
 				writer.close();
 				fromServer.close();
 				inputStream.close();
 				inputStreamRe.close();
 				socketClient.close();
 				in.close();
-			}
-			catch(IOException exc)
-			{
+			} catch (IOException exc) {
 				System.out.println("Exception while exiting: " + exc.getMessage());
 			}
 			System.out.println("Connection is closed.");
 			System.exit(0);
 		}
-		while(!command.contains("EXIT"))
-		{
-			if(command.contains("UPDT"))
-			{
-				String responseFromServer =  updateTheVersion(myObje.getVersionOfClient());
-				System.out.println(responseFromServer);
-				if(responseFromServer.contains(response[0]) && !responseFromServer.contains(response[1]))
-				{
+		while (!command.contains("EXIT")) {
+			// UPDATE
+			if (command.contains("UPDT")) {
+				writer.println(command);
+				String responseFromServer = null;
+				try {
+					responseFromServer = fromServer.readLine();
+					System.out.println(responseFromServer);
+				} catch (IOException exc) {
+					System.out.println("about update: " + exc.getMessage());
+				}
+				if (responseFromServer.contains(response[0]) && !responseFromServer.contains(response[1])) {
 					int versionCurrent = myObje.getVersionOfClient();
 					versionCurrent++;
 					myObje.setVersionOfClient(versionCurrent);
-				}
-				else
-				{
+				} else {
 					System.out.println("The other functions can be used.");
 				}
 			}
-			else if(command.contains("WRTE"))
-			{
+			// WRITE
+			else if (command.contains("WRTE")) {
 				writer.println(command);
-				String responseFromServerWrite = null; 
-				try
-				{
+				String responseFromServerWrite = null;
+				try {
 					responseFromServerWrite = fromServer.readLine();
 					System.out.println(responseFromServerWrite);
-				}
-				catch (IOException exc) {
+				} catch (IOException exc) {
 					System.out.println("About write command: " + exc.getMessage());
 				}
-				if(responseFromServerWrite.contains(response[0]) && !responseFromServerWrite.contains(response[1]))
-				{
-					int versionCurrent = myObje.getVersionOfClient();
-					versionCurrent++;
-					myObje.setVersionOfClient(versionCurrent);
-				}
 			}
-			else if(command.contains("APND"))
-			{
+			// APPEND
+			else if (command.contains("APND")) {
 				writer.println(command);
 				String responseFromServerapn = null;
-				try
-				{
+				try {
 					responseFromServerapn = fromServer.readLine();
 					System.out.println(responseFromServerapn);
-				}
-				catch (IOException exc) {
-					System.out.println("About append: " +exc.getMessage());
-				}
-				System.out.println(responseFromServerapn);
-				if(responseFromServerapn.contains(response[0]) && !responseFromServerapn.contains(response[1]))
-				{
-					int versionCurrent = myObje.getVersionOfClient();
-					versionCurrent++;
-					myObje.setVersionOfClient(versionCurrent);
+				} catch (IOException exc) {
+					System.out.println("About append: " + exc.getMessage());
 				}
 			}
 			System.out.println("Please enter a command to execute: ");
 			command = in.nextLine();
 		}
-		try
-		{
+		// CLOSING OF THE PROGRAM
+		try {
 			writer.close();
 			fromServer.close();
 			inputStream.close();
 			inputStreamRe.close();
 			socketClient.close();
 			in.close();
-		}
-		catch(IOException exc)
-		{
+		} catch (IOException exc) {
 			System.out.println("Exception while exiting: " + exc.getMessage());
 		}
 		System.out.println("Connection is closed.");
 		System.exit(0);
 	}
-	public static boolean sendUserCommand(String usernameClient) 
-	{
+
+	/**
+	 * This method is used to send USER command with username and get the response
+	 * from the server
+	 * 
+	 * @param String username numA This is the first parameter
+	 * @return boolean if the username is OK or INVALID
+	 */
+	public static boolean sendUserCommand(String usernameClient) {
 		writer.println(usernameClient);
 		String[] response = { "OK", "INVALID" };
 		try {
@@ -226,8 +216,14 @@ public class TextEditor {
 		return false;
 	}
 
-	public static boolean sendPassCommand(String passwordClient)
-	{
+	/**
+	 * This method is used to send PASS command with password and get the response
+	 * from the server
+	 * 
+	 * @param String password This is the first parameter
+	 * @return boolean if the password is OK or INVALID
+	 */
+	public static boolean sendPassCommand(String passwordClient) {
 		writer.println(passwordClient);
 		String[] response = { "OK", "INVALID" };
 		try {
@@ -239,25 +235,13 @@ public class TextEditor {
 		}
 		return false;
 	}
-	public static String updateTheVersion(int versionCurrent)
-	{
-		writer.println("UPDT " + versionCurrent);
-		try
-		{
-			String responseFromServer = fromServer.readLine();
-			System.out.println(responseFromServer);
-			return responseFromServer;
-		}
-		catch (IOException exc) {
-			return exc.getMessage();
-		}
-	}
-	public int getVersionOfClient()
-	{
+
+	// KEEPING THE VERSION, JUST IN CASE
+	public int getVersionOfClient() {
 		return this.version;
 	}
-	public void setVersionOfClient(int versionCurrent)
-	{
+
+	public void setVersionOfClient(int versionCurrent) {
 		this.version = versionCurrent;
 	}
 }
